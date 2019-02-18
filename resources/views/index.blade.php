7f0925bc2c16fe518687b0d9977592fd3228086c
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2019-02-18 16:09:48 +0100

    concluso html e stile header versione base

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,18 +7,18 @@
   </head>
   <body>
     <header>
-      <div class="container">
+      <div class="container-fluid">
         <div class="row">
           <div class="col-6 wrapLogo">
             <img class="logo" src="{{ asset('img/logo.png') }}" alt="">
           </div>
           <div class="col-6 wrapMenu">
             <ul>
-              <li class="elemento">HOME</li>
-              <li class="elemento">CORSO</li>
-              <li class="elemento">METODO</li>
-              <li class="elemento">DOPO IL CORSO</li>
-              <li class="elemento">FAQ</li>
+              <li class="elemento"><a href="#">HOME</a></li>
+              <li class="elemento"><a href="#">CORSO</a></li>
+              <li class="elemento"><a href="#">METODO</a></li>
+              <li class="elemento"><a href="#">DOPO IL CORSO</a></li>
+              <li class="elemento"><a href="#">FAQ</a></li>
             </ul>
           </div>
         </div>
