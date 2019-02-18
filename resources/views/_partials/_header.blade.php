047ad23a96e36a3cdcfc75ad95857391a8db6f47
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2019-02-18 16:13:27 +0100

    incapsulato l'header e richiamato con @include

---
+++ resources/views/_partials/_header.blade.php
@@ -0,0 +1,18 @@
+<header>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-6 wrapLogo">
+        <img class="logo" src="{{ asset('img/logo.png') }}" alt="">
+      </div>
+      <div class="col-6 wrapMenu">
+        <ul>
+          <li class="elemento"><a href="#">HOME</a></li>
+          <li class="elemento"><a href="#">CORSO</a></li>
+          <li class="elemento"><a href="#">METODO</a></li>
+          <li class="elemento"><a href="#">DOPO IL CORSO</a></li>
+          <li class="elemento"><a href="#">FAQ</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</header>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,23 +6,6 @@
     <title>index</title>
   </head>
   <body>
-    <header>
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-6 wrapLogo">
-            <img class="logo" src="{{ asset('img/logo.png') }}" alt="">
-          </div>
-          <div class="col-6 wrapMenu">
-            <ul>
-              <li class="elemento"><a href="#">HOME</a></li>
-              <li class="elemento"><a href="#">CORSO</a></li>
-              <li class="elemento"><a href="#">METODO</a></li>
-              <li class="elemento"><a href="#">DOPO IL CORSO</a></li>
-              <li class="elemento"><a href="#">FAQ</a></li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </header>
+    @include('_partials._header')
   </body>
 </html>
