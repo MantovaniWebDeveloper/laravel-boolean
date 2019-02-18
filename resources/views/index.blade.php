9cc577effdc6ee15bbdea354017a889cb41edb90
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2019-02-18 15:54:20 +0100

    inserito logo nel header e il menu

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,10 +10,16 @@
       <div class="container">
         <div class="row">
           <div class="col-6 wrapLogo">
-
+            <img class="logo" src="{{ asset('img/logo.png') }}" alt="">
           </div>
           <div class="col-6 wrapMenu">
-            
+            <ul>
+              <li class="elemento">HOME</li>
+              <li class="elemento">CORSO</li>
+              <li class="elemento">METODO</li>
+              <li class="elemento">DOPO IL CORSO</li>
+              <li class="elemento">FAQ</li>
+            </ul>
           </div>
         </div>
       </div>
