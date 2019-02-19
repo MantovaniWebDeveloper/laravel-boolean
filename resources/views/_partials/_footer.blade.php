5a1a926e5d9cd8a5785212036e0a190849d38119
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2019-02-19 11:31:13 +0100

    inizio parte centrale del footer

--- resources/views/_partials/_footer.blade.php
+++ resources/views/_partials/_footer.blade.php
@@ -4,7 +4,25 @@
       <div class="topFooterContent text-center">
         <h3>Sei ad un passo dalla tua nuova professione</h3>
         <p>Scrivici, raccontaci la tua storia e scopriamo insieme se Boolean è la scelta giusta per te</p>
-        <input type="text" name="" value="Inizia da qui">
+        <input class ="btnIscriviti" type="text" name="" value="Inizia da qui">
+      </div>
+    </div>
+  </div>
+  <div class="centerFooter">
+    <div class="container">
+      <div class="row">
+        <div class="col-3">
+          <img class="img-fluid"src="{{ asset('img/logo.png') }}" alt="logo">
+        </div>
+        <div class="col-3">
+
+        </div>
+        <div class="col-3">
+
+        </div>
+        <div class="col-3">
+
+        </div>
       </div>
     </div>
   </div>
