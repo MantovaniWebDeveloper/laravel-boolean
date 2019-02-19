d53d2bb385ad0fd833ff549b4b77b6d190ead928
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2019-02-19 10:56:43 +0100

    finito il jumbotron non proprio identitico

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,16 +3,21 @@
 @section('segnapostoContent')
 
   @include('_partials._header')
+  <div class="retGreen">
+  </div>
   <div class="container">
     <div class="row">
       <div class="col-5 wrapTextPromo">
         <div class="content">
           <h2>Diventa sviluppatore web</h2>
           <p>6 mesi di corso intensivo online in diretta per imparare da zero il mestiere di web developer e trovare un nuovo lavoro nelle aziende top del settore</p>
+          <input class="btnScopri" type="submit" name="" value="SCOPRI IL CORSO">
         </div>
       </div>
       <div class="col-7 wrapImgPromo">
-
+        <div class="content">
+          <img class="img-fluid" src="{{ asset('img/mac2x.png')}}" alt="">
+        </div>
       </div>
     </div>
   </div>
