d242db297868acd561229b3935aa89d924449bfa
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2019-02-20 15:17:38 +0100

    creato il primo box per studente in maniera statica

--- resources/views/studenti.blade.php
+++ resources/views/studenti.blade.php
@@ -3,7 +3,22 @@
 @section('segnapostoContent')
 
   @include('_partials._header')
-  
+
+  <div class="container">
+    <div class="row">
+      <div class="col-6">
+        <div class="wrapStudente">
+          <div class="wrapImgStudente">
+            <img src="https://www.boolean.careers/images/students/biagini.png" alt="">
+          </div>
+          <div class="wrapContentStudente">
+            <h3>Alessandro Biagini</h3>
+            <p>Ora è Web Developer in Softec spa</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 
   @include('_partials._footer')
 
